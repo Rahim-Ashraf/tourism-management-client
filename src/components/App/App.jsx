23ebc88208693edbbf836e351f75863bd9942fda
Abdur Rahim <rahim11500000@gmail.com>
2024-04-25 20:03:11 +0600

    make navbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../Navbar/Navbar";
 
 
 const App = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h1>Tourism Management</h1>
         </div>
     );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Link, NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <Link className="btn btn-ghost text-xl">Dream Tour</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <NavLink>Home</NavLink>
+                    <NavLink>All Tourists Spot</NavLink>
+                    <NavLink>Add Tourists Spot</NavLink>
+                    <NavLink>My List</NavLink>
+                </ul>
+            </div>
+            <div className="navbar-end gap-2">
+                <Link to={"/login"} className="btn">Login</Link>
+                <Link to={"/register"} className="btn">Register</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
